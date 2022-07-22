b7cf2544386d308b84f490d9067287bd5d7ac79f
tiwarisun <suniltiwari460@gmail.com>
2022-07-22 18:02:03 +0530

    jm

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ const serverDetails= {
     runningPort: process.env.PORT || 3000
 }
 
-app.listen(serverDetails.runningPort, function () {
-    console.log('Express app running on port ' + (serverDetails.runningPort))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
 
 module.exports.serverDetails = serverDetails
